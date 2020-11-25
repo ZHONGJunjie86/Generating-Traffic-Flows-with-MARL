2081877db6f292c77e3ece6d9b30de497da63362
Your Name <is0541hx@ed.ritsumei.ac.jp>
2020-11-25 22:47:29 +0900

    architecture change

--- CV_input.py
+++ CV_input.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os 
 import cv2
+import  io
+#from PIL import Image
+#import skimage
+#from skimage import io as skio
 
 file_head = os.getcwd()+'\\GAMA_python\\Generate_Traffic_Flow_MAS_RL\\GAMA_img\\'
 save_img = os.getcwd()+'\\GAMA_python\\Generate_Traffic_Flow_MAS_RL\\GAMA_img\\save_agents.png'
@@ -104,9 +108,124 @@ def generate_img():
             except(IndexError,FileNotFoundError,ValueError,OSError,PermissionError):
                 error = True
         
-        plt.savefig(save_img, dpi=100)
+        plt.savefig(save_img, dpi=100,format = 'png')
         plt.close()
-        img_cv = cv2.imread(save_img)   # cv2.imread()------np.array, (H x W xC), [0, 255], BGR
+      
+        img_cv = cv2.imread(save_img)
         img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
         list_img.append(img_cv) 
+        
+        #buffer = io.BytesIO()
+        #plt.savefig(buffer, format = 'png') #dpi=100,
+        #buffer.seek(0) 
+        #dataPIL = Image.open(buffer)
+        #img_cv = np.asarray(dataPIL)#(dataPIL)
+        #img_cv = cv2.imread(save_img)   # cv2.imread()------np.array, (H x W xC), [0, 255], BGR\
+        #img_cv = skio.imread(buffer)
+        #img_cv = np.asarray(img_cv)
+        #buffer.close()
+
     return list_img
+
+def generate_img_train(): 
+    location_NPC_front = os.getcwd()+'/Generate_Traffic_Flow_MAS_RL/GAMA_img/train/save_NPC_front.csv'
+    location_NPC_behind = os.getcwd()+'/Generate_Traffic_Flow_MAS_RL/GAMA_img/train/save_NPC_behind.csv'
+    location_NPC_closest_10 = os.getcwd()+'/Generate_Traffic_Flow_MAS_RL/GAMA_img/train/save_NPC_closest_10.csv'
+    location_route = os.getcwd()+'/Generate_Traffic_Flow_MAS_RL/GAMA_img/train/route.csv'
+    location_self =os.getcwd()+"/Generate_Traffic_Flow_MAS_RL/GAMA_img/train/save_self.csv"
+    save_img = os.getcwd()+'/Generate_Traffic_Flow_MAS_RL/GAMA_img/train/save_agents.png'
+    error = True
+    while error == True:
+        try:
+            NPC_front = []
+            NPC_behind = []
+            NPC_closest_10 = []
+            Route = []
+            SELF = []
+            with open(location_NPC_front)as f:
+                f_csv = csv.reader(f)
+                for i in f_csv:
+                    NPC_front = i
+            with open(location_NPC_behind)as f:
+                f_csv = csv.reader(f)
+                for i in f_csv:
+                    NPC_behind = i
+            with open(location_NPC_closest_10)as f:
+                f_csv = csv.reader(f)
+                for i in f_csv:
+                    NPC_closest_10 = i
+            with open(location_route)as f:
+                f_csv = csv.reader(f)
+                for i in f_csv:
+                    Route = i
+            with open(location_self)as f:
+                f_csv = csv.reader(f)
+                for i in f_csv:
+                    SELF = i
+
+            NPC_front_X = []
+            NPC_front_Y = []
+            NPC_behind_X = []
+            NPC_behind_Y = []
+            NPC_closest_10_X = []
+            NPC_closest_10_Y = []
+            Route_X = []
+            Route_Y = []
+            count = 1
+            for i in NPC_front:
+                if count == 1 :
+                    NPC_front_X.append(float(i[1:]))
+                elif(count == 2):
+                    NPC_front_Y.append(float(i))
+                else:
+                    count = 0
+                count += 1
+
+            for i in NPC_behind:
+                if count == 1 :
+                    NPC_behind_X.append(float(i[1:]))
+                elif(count == 2):
+                    NPC_behind_Y.append(float(i))
+                else:
+                    count = 0
+                count += 1
+            for i in NPC_closest_10:
+                if count == 1 :
+                    NPC_closest_10_X.append(float(i[1:]))
+                elif(count == 2):
+                    NPC_closest_10_Y.append(float(i))
+                else:
+                    count = 0
+                count += 1
+            for i in Route:
+                if count == 1 :
+                    try:
+                        Route_X.append(float(i[1:]))
+                    except(ValueError):
+                        Route_X.append(float(i[2:]))
+                elif(count == 2):
+                    Route_Y.append(float(i))
+                else:
+                        count = 0
+                count += 1
+            plt.figure(figsize=(5, 5)) #500*500
+            plt.axis('off') 
+            plt.xlim(float(SELF[0][1:])-100,float(SELF[0][1:])+100)
+            plt.ylim(float(SELF[1])-100, float(SELF[1])+100)
+            plt.scatter(Route_X[0],Route_Y[0],color = 'g',marker = 'h') #start
+            plt.scatter(Route_X[len(Route_X)-1],Route_Y[len(Route_Y)-1],color = 'purple',marker = 'h') #end
+            plt.plot(Route_X,Route_Y,color = 'grey',alpha=0.7)   #route
+            plt.scatter(NPC_behind_X, NPC_behind_Y,color = 'b',marker = 'o',s=12) #NPC_behind
+            plt.scatter(NPC_front_X, NPC_front_Y,color = 'c',marker = '>',s=12) #NPC_front
+            plt.scatter(NPC_closest_10_X, NPC_closest_10_Y,color = 'm',marker = 'P',s=12) #NPC_10
+            plt.scatter(float(SELF[0][1:]), float(SELF[1]),color = 'r',marker= 'D',s=12)
+            error = False
+        except(IndexError,FileNotFoundError,ValueError,OSError,PermissionError):
+            error = True
+    
+    plt.savefig(save_img, dpi=100)
+    plt.close()
+    img_cv = cv2.imread(save_img)   # cv2.imread()------np.array, (H x W xC), [0, 255], BGR
+    img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    return img_cv
+
--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ from_GAMA_3 = os.getcwd()+'\\GAMA_python\\Generate_Traffic_Flow_MAS_RL\\GAMA_R\\
 from_python_1 = os.getcwd()+'\\GAMA_python\\Generate_Traffic_Flow_MAS_RL\\GAMA_R\\python_AC_1.csv'
 from_python_2 = os.getcwd()+'\\GAMA_python\\Generate_Traffic_Flow_MAS_RL\\GAMA_R\\python_AC_2.csv'
 
+D_A_T = os.getcwd()+'/Generate_Traffic_Flow_MAS_RL/GAMA_R/D_A_T.csv'
+
 def reset():
     f=open(from_GAMA_1, "r+")
     f.truncate()
@@ -20,6 +22,8 @@ def reset():
     f.truncate()
     f=open(from_python_2, "r+")
     f.truncate()
+    f4=open(D_A_T, "r+")
+    f4.truncate()
     return_ = [0]
     np.savetxt(from_python_1,return_,delimiter=',')
     np.savetxt(from_python_2,return_,delimiter=',')
@@ -51,23 +55,18 @@ def send_to_GAMA(to_GAMA):
             error = True 
 
 #[real_speed/10, target_speed/10, elapsed_time_ratio, distance_left/100,distance_front_car/10,distance_behind_car/10,reward,done,over]
-def GAMA_connect(test):
+def GAMA_connect(test=0):
     error = True
     while error == True:
         try:
-            time.sleep(0.003)
             state_1 = np.loadtxt(from_GAMA_1, delimiter=",")
             state_2 = np.loadtxt(from_GAMA_2, delimiter=",")
             state_3 = np.loadtxt(from_GAMA_3, delimiter=",")
-            time_pass = state_1[2];time_pass = state_2[2];time_pass = state_3[2]
+            A_T= np.loadtxt(D_A_T, delimiter=",")
+            time_pass = state_1[2];time_pass = state_2[2];time_pass = state_3[2];test = A_T[1]
             error = False
         except (IndexError,FileNotFoundError,ValueError,OSError):
-            time.sleep(0.003)
             error = True
-        
-    state_1 = np.delete(state_1, [6,7,8], axis = 0) #4,5,
-    state_2 = np.delete(state_2, [6,7,8], axis = 0)
-    state_3 = np.delete(state_3, [6,7,8], axis = 0)
 
     error = True
     while error == True:
@@ -78,10 +77,25 @@ def GAMA_connect(test):
             f2.truncate()
             f3=open(from_GAMA_3, "r+")
             f3.truncate()
+            f4=open(D_A_T, "r+")
+            f4.truncate()
             error = False
 
         except (IndexError,FileNotFoundError,ValueError,OSError):
             time.sleep(0.003)
             error = True
-    state = [state_1,state_2,state_3]
-    return state#,reward,done,time_pass,over,
+
+   
+    if  A_T[0] == 0: 
+        time_pass = state_1[2]
+        reward = state_1[6]
+        done = state_1[7]  
+        over = state_1[8] 
+        state = np.delete(state_1, [2,3,5,6,7,8], axis = 0) #4,5,  # 3!!!!
+        return 0,state,reward,done,time_pass,over,
+    elif  A_T[0] == 1: 
+        state_1 = np.delete(state_1, [2,3,5,6,7,8], axis = 0) #4,5,
+        state_2 = np.delete(state_2, [2,3,5,6,7,8], axis = 0)
+        state_3 = np.delete(state_3, [2,3,5,6,7,8], axis = 0)
+        state = [state_1,state_2,state_3]
+        return 1,state,0,0,0,0#,reward,done,time_pass,over,
